0cbd225a0c418ca8f8526e228c2a0abebad2cf4f
Cannon Moyer <cannonmoyer@yahoo.com>
2017-04-30 16:51:59 +0000

    final commit before production push to heroku

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,6 +26,7 @@ $(document).ready(function(){
     $( "#close_menu" ).click(function() {
        mobileMenu.style.display = "none";
     });
+    
 });
 $(document).ready(function(){
     
@@ -45,44 +46,13 @@ $(document).ready(function(){
 
 // Get the button that opens the modal
 
-var btn3 = document.getElementById("myBtn3");
-var btn4 = document.getElementById("myBtn4");
-var btn5 = document.getElementById("myBtn5");
-var btn6 = document.getElementById("myBtn6");
-var btn7 = document.getElementById("myBtn7");
-var btn8 = document.getElementById("myBtn8");
+
 
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal
 
-btn3.onclick = function() {
-    modal.style.display = "block";
-}
-btn4.onclick = function() {
-    modal.style.display = "block";
-}
-btn5.onclick = function() {
-    modal.style.display = "block";
-}
-btn6.onclick = function() {
-    modal.style.display = "block";
-}
-btn7.onclick = function() {
-    modal.style.display = "block";
-}
-btn8.onclick = function() {
-    modal.style.display = "block";
-}
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
 
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+// When the user clicks on <span> (x), close the modal
+
